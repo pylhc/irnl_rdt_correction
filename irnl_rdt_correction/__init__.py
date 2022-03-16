98927fbe396e415d4903ce30637a02e25e33c572
JoschD <26184899+JoschD@users.noreply.github.com>
2022-03-16 16:12:09 +0100

    Copied from pylhc

---
+++ irnl_rdt_correction/__init__.py
@@ -0,0 +1,18 @@
+"""
+IRNL RDT Correction
+~~~~~~~~~~~~~~~~~~~
+
+Correction script to power the nonlinear correctors in the (HL-)LHC insertion regions based on RDTs.
+
+:copyright: pyLHC/OMC-Team working group.
+:license: MIT, see the LICENSE.md file for details.
+"""
+__title__ = "irnl_rdt_correction"
+__description__ = "Correction script to power the nonlinear correctors in the (HL-)LHC insertion regions based on RDTs."
+__url__ = "https://github.com/pylhc/irnl_rdt_correction"
+__version__ = "0.0.0"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
+__all__ = [__version__]
---
+++ irnl_rdt_correction/__main__.py
@@ -0,0 +1,5 @@
+from irnl_rdt_correction.irnl_rdt_correction import main, log_setup
+
+if __name__ == '__main__':
+    log_setup()
+    main()
