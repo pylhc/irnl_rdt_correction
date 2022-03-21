0a4396e1ec2b5adc9d4794bda813185b93698da9
JoschD <26184899+JoschD@users.noreply.github.com>
2022-03-21 14:43:59 +0100

    bump 0.0.1

--- irnl_rdt_correction/__init__.py
+++ irnl_rdt_correction/__init__.py
@@ -10,7 +10,7 @@ Correction script to power the nonlinear correctors in the (HL-)LHC insertion re
 __title__ = "irnl-rdt-correction"
 __description__ = "Correction script to power the nonlinear correctors in the (HL-)LHC insertion regions based on RDTs."
 __url__ = "https://github.com/pylhc/irnl_rdt_correction"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
