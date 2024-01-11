591f831d0775ad1b119397582bda0c24a7badeb9
JoschD <26184899+JoschD@users.noreply.github.com>
2024-01-11 14:33:24 +0100

    fix absolute import

--- irnl_rdt_correction/__main__.py
+++ irnl_rdt_correction/__main__.py
@@ -1,5 +1,5 @@
 from irnl_rdt_correction.main import irnl_rdt_correction
-from utilities import log_setup
+from irnl_rdt_correction.utilities import log_setup
 
 if __name__ == '__main__':
     log_setup()
